39d211b0547646ff89d82977b617003aa3d2188b
sleekcharly <c_ukasoanya@yahoo.com>
2024-12-07 23:55:28 +0100

    COnfigured tailwindCSS, imported code snippets and structured application for build

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function SignUp() {
+  return (
+    <SafeAreaView>
+      <Text>Sign Up</Text>
+    </SafeAreaView>
+  );
+}
