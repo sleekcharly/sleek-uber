276f1f15a6cb5c7c774043f1a36ad4cb4192f0c1
sleekcharly <c_ukasoanya@yahoo.com>
2025-02-02 21:43:43 +0100

    cretaed the various pages for driver and ride confirmation

---
+++ components/Payment.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import CustomButton from './CustomButton';
+
+const Payment = () => {
+  const openPaymentSheet = async () => {};
+
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </>
+  );
+};
+
+export default Payment;
--- lib/map.ts
+++ lib/map.ts
@@ -37,8 +37,8 @@ export const calculateRegion = ({
 }) => {
   if (!userLatitude || !userLongitude) {
     return {
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: 6.5276,
+      longitude: 3.21093,
       latitudeDelta: 0.01,
       longitudeDelta: 0.01,
     };
